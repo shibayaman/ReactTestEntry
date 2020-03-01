d1e34d1a15656e094d81b3a89006d21ed608bc67
shibayaman <shibayaman7374@gmail.com>
2020-03-01 23:25:44 +0900

    Add todoModelのget系の単体テスト

--- src/backend/__test__/todoApis.test.js
+++ src/backend/__test__/todoApis.test.js
@@ -1,7 +1,50 @@
 const api = require('../todoModel');
+const data = require('../todoData');
 
-describe('all', () => {
-  it('should return all todos in an array', () => {
-    expect(1).toBe(1);
+beforeEach(() => {
+  data.index = 5;
+  data.todo = [
+    { id: 0, task: '寝る', due: '今すぐ', priority: 'とても高い' },
+    { id: 2, task: '起きる', due: '5年後', priority: '地より低い' },
+    { id: 4, task: '食べる', due: '5秒後', priority: '命より大事' }
+  ];
+});
+
+describe('getIndexById', () => {
+  it('should search todo by id', () => {
+    const id = 4;
+    const expectedIndex = 2;
+
+    expect(api.getIndexById(id)).toBe(expectedIndex);
+  });
+  it('should return null when todo is not found', () => {
+    const id = 10;
+
+    expect(api.getTodoById(id)).toBeNull();
   })
+});
+
+describe('getAllTodos', () => {
+  it('should return all todos in the array', () => {
+    expect(api.getAllTodos()).toEqual([
+      { id: 0, task: '寝る', due: '今すぐ', priority: 'とても高い' },
+      { id: 2, task: '起きる', due: '5年後', priority: '地より低い' },
+      { id: 4, task: '食べる', due: '5秒後', priority: '命より大事' }
+    ]);
+  });
+});
+
+describe('getTodoById', () => {
+  it('should return todo with given id', () => {
+    const id = 2;
+    const expectedTodo = { id: 2, task: '起きる', due: '5年後', priority: '地より低い' };
+
+    expect(api.getTodoById(id)).toEqual(expectedTodo);
+  });
+
+  it('should return null when todo is not found', () => {
+    const id = 1;
+
+    expect(api.getTodoById(id)).toBeNull();
+  });
 });
--- src/backend/todoModel.js
+++ src/backend/todoModel.js
@@ -62,5 +62,6 @@ module.exports = {
   getTodoById,
   createTodo,
   updateTodo,
-  deleteTodo
+  deleteTodo,
+  getIndexById
 }
